3b206b84d9762e1b3a12eed7b1ca322d90ccea89
ktgangoso <kevin.gangoso@gmail.com>
2025-03-28 18:47:33 +0800

    fix responsive design

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -23,8 +23,8 @@ function News() {
                         />
                     </Box>
                     <Box sx={{ display: "flex", justifyContent: "space-around", textAlign: "left", alignItems: "start", flexDirection: "column", gap: "1rem" }}>
-                        <Typography sx={{ fontSize: "1rem", fontWeight: "700" }}>HEALTHY FOOD</Typography>
-                        <Typography sx={{ fontSize: { xs: ".8rem", md: "1rem" }, fontWeight: "700" }}>5 High-Protein Foods to Help You Build Strength and Muscle</Typography>
+                        <Typography sx={{ fontSize: "1rem", fontWeight: "500" }}>HEALTHY FOOD</Typography>
+                        <Typography sx={{ fontSize: { xs: ".8rem", md: "2rem" }, fontWeight: "700" }}>5 High-Protein Foods to Help You Build Strength and Muscle</Typography>
                         <Typography sx={{ fontSize: { xs: ".8rem", md: "1rem" }, }}>Including the right protein-rich foods in your diet is essential for muscle growth and overall strength.</Typography>
                         <Button
                             sx={{
@@ -59,7 +59,7 @@ function News() {
                     flexDirection: { xs: "column", md: "column" }, width: "auto", gap: "1rem", backgroundColor: "#fff",
                 }}>
 
-                    <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: {xs: "column", md: "row"}, gap: "1rem", width: "100%"}}>
+                    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: { xs: "column", md: "row" }, gap: "1rem", width: "100%" }}>
                         <Typography sx={{ fontSize: { xs: "", md: "1.2rem" }, fontWeight: "700" }} >Other Article</Typography>
                         <Divider sx={{ width: { xs: "100%", md: "80%" }, border: "1px solid black" }} />
                     </Box>
@@ -67,7 +67,7 @@ function News() {
                     <Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center", flexDirection: { xs: "column", md: "row" }, gap: "1rem", }}>
                         <Box sx={{
                             border: "2px solid rgba(0, 0, 0, 0.3)",
-                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", margin: "1rem", width: { xs: "18rem", md: "25rem" }, height: "100%"
+                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", margin: "1rem", width: { xs: "18rem", md: "25rem" }, height: { xs: "100%", md: "33rem" }
                         }}>
                             <CardMedia
                                 sx={{ height: { xs: "18rem", md: "20rem" }, width: { xs: "18rem", md: "25rem" } }}
@@ -111,35 +111,94 @@ function News() {
 
                         <Box sx={{
                             border: "2px solid rgba(0, 0, 0, 0.3)",
-                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", margin: "1rem", width: { xs: "18rem", md: "25rem" }, height: { xs: "100%", md: "33rem" }
                         }}>
                             <CardMedia
-                                sx={{ height: { xs: "18rem", md: "25rem" }, width: { xs: "18rem", md: "25rem" } }}
+                                sx={{ height: { xs: "18rem", md: "20rem" }, width: { xs: "18rem", md: "25rem" } }}
                                 component="img"
                                 image="/img/nutritionnews.webp"
                                 alt=""
                             />
-                            <Box sx={{ padding: ".5rem" }}>
-                                <Typography>Title News 1</Typography>
-                                <Typography>Title News 1</Typography>
-                                <Typography>Title News 1</Typography>
+                            <Box sx={{ padding: ".5rem", textAlign: "left" }}>
+                                <Typography sx={{ fontSize: { xs: "1rem", md: "1rem" }, fontWeight: "700" }}>
+                                    High-Protein Foods: Fuel Your Body for Strength and Fat Loss
+                                </Typography>
+                                <Typography sx={{ fontSize: { xs: ".8rem", md: ".8rem" } }}>Adding high-protein foods like lean meats, eggs, legumes,
+                                    and dairy to your diet helps build muscle, boost metabolism, and keep you full longer.
+                                    These nutrient-rich options support fat loss and overall health by reducing cravings and preserving lean body mass.
+                                </Typography>
+                                <Button
+                                    sx={{
+                                        color: "#000",
+                                        position: "relative",
+                                        "&::after": {
+                                            content: '""',
+                                            position: "absolute",
+                                            width: "0%",
+                                            height: "2px",
+                                            bottom: 0,
+                                            left: 0,
+                                            backgroundColor: "black",
+                                            transition: "width .3s linear"
+                                        },
+                                        "&:hover": {
+                                            color: "#333",
+                                            borderRadius: "0",
+                                            "&::after": {
+                                                width: "100%"
+                                            }
+                                        }
+                                    }}
+                                >
+                                    Read Article
+                                </Button>
                             </Box>
                         </Box>
 
                         <Box sx={{
                             border: "2px solid rgba(0, 0, 0, 0.3)",
-                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", margin: "1rem", width: { xs: "18rem", md: "25rem" }, height: { xs: "100%", md: "33rem" }
                         }}>
                             <CardMedia
-                                sx={{ height: { xs: "18rem", md: "25rem" }, width: { xs: "18rem", md: "25rem" } }}
+                                sx={{ height: { xs: "18rem", md: "20rem" }, width: { xs: "18rem", md: "25rem" } }}
                                 component="img"
                                 image="/img/nutritionnews.webp"
                                 alt=""
                             />
-                            <Box sx={{ padding: ".5rem" }}>
-                                <Typography>Title News 1</Typography>
-                                <Typography>Title News 1</Typography>
-                                <Typography>Title News 1</Typography>
+                            <Box sx={{ padding: ".5rem", textAlign: "left" }}>
+                                <Typography sx={{ fontSize: { xs: "1rem", md: "1rem" }, fontWeight: "700" }}>
+                                    Foods to Avoid for Effective Weight Loss
+                                </Typography>
+                                <Typography sx={{ fontSize: { xs: ".8rem", md: ".8rem" } }}>Steer clear of processed foods, sugary drinks,
+                                    and refined carbs, as they contribute to excess calorie intake and spikes in blood sugar.
+                                    Avoiding these unhealthy options helps prevent cravings, supports fat loss, and promotes overall well-being.
+                                </Typography>
+                                <Button
+                                    sx={{
+                                        color: "#000",
+                                        position: "relative",
+                                        marginTop: {xs: "0", md: "2.5rem"},
+                                        "&::after": {
+                                            content: '""',
+                                            position: "absolute",
+                                            width: "0%",
+                                            height: "2px",
+                                            bottom: 0,
+                                            left: 0,
+                                            backgroundColor: "black",
+                                            transition: "width .3s linear"
+                                        },
+                                        "&:hover": {
+                                            color: "#333",
+                                            borderRadius: "0",
+                                            "&::after": {
+                                                width: "100%"
+                                            }
+                                        }
+                                    }}
+                                >
+                                    Read Article
+                                </Button>
                             </Box>
                         </Box>
 
